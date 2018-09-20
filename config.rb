b617aba9e3cb7446c9898a5d1e25449f31b22a3b
Majid Jaidi <majid.jaidi@gmail.com>
2018-09-20 14:43:45 +0100

    config middleman blog

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ activate :sprockets
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/*.md', layout: false
 
 configure :build do
   activate :minify_css
@@ -19,4 +20,29 @@ end
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
+end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "projets"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "{year}-{month}-{day}_{title}"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
 end
